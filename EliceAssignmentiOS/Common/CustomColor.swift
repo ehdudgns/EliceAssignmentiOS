a43b69f792e749d9115d680c7ef33d6906ab3239
ehdudgnsTW <brown@twinny.co.kr>
2024-02-21 12:31:46 +0900

    [추가] 디자인에 나와있는 컬러들을 저장하였습니다.

---
+++ EliceAssignmentiOS/Common/CustomColor.swift
@@ -0,0 +1,25 @@
+//
+//  CustomColor.swift
+//  EliceAssignmentiOS
+//
+//  Created by 도영훈 on 2024/02/20.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let eliceDarkGray = UIColor(red: 58/255, green: 58/255, blue: 76/255, alpha: 1)
+    
+    static let eliceLightGray = UIColor(red: 228/255, green: 228/255, blue: 228/255, alpha: 1)
+    
+    static let elicePrimary = UIColor(red: 90/255, green: 46/255, blue: 204/255, alpha: 1)
+    
+    static let eliceRed = UIColor(red: 244/255, green: 67/255, blue: 54/255, alpha: 1)
+    
+    static let defaultLogoBackgroundColor = UIColor(red: 243/255, green: 243/255, blue: 243/255, alpha: 1)
+    
+    static let detailViewSmallTitleColor = UIColor(red: 82/255, green: 79/255, blue: 161/255, alpha: 1)
+    
+    static let dividViewBackgroundColor = UIColor(red: 174/255, green: 174/255, blue: 174/255, alpha: 1)
+}
